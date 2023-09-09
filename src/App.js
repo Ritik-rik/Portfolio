3f4835d96de6bc7e8ccedcf4b7433c7147491f91
Ritik <mehta7b@uwindsor.ca>
2023-09-10 01:54:46 +0530

    fixed_e

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   <>
   <Routes>
 
-    <Route path="/" element={<Home/>} />  //routing pages
+    <Route path="/" element={<Home/>} /> 
     <Route path="/project" element={<Project/>} /> 
     <Route path="/about" element={<About/>} /> 
     <Route path="/contact" element={<Contact/>} /> 
