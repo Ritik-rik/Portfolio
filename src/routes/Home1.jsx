baf86d10180fc1014631a77fd5e0b9da0017c2fd
Socialdukan <mehta7b@uwindsor.ca>
2025-03-02 22:56:46 -0500

    Before integ Angular

---
+++ src/routes/Home1.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home1 = () => {
+  return (
+    <div>
+      
+      <h2>Welcome, Recruiter!</h2>
+      <p>This is your recruiter-specific content.</p>
+      
+    </div>
+  );
+};
+
+export default Home1;
