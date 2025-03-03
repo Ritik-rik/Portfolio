b31baacc59638f5bf348e100e33a75835a1de226
Socialdukan <mehta7b@uwindsor.ca>
2025-03-03 00:55:26 -0500

    Setup workspace and migrated react to wx

---
+++ my-portfolio/apps/react-portfolio/src/routes/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Heroimg from '../components/Heroimg';
+import Footer from '../components/Footer';
+import Work from '../components/Work';
+import Exp from '../components/Exp'
+const Home = () => {
+  return  <div>  <Navbar/>
+      <Heroimg />
+      <Work/>
+     
+      <Exp/>
+      <Footer />
+   </div>
+  
+}
+
+export default Home
