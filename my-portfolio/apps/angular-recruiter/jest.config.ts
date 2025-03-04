1f1b095d032d52afa4141228488386120116c92b
Socialdukan <mehta7b@uwindsor.ca>
2025-03-03 19:21:07 -0500

    Step to start angular

--- my-portfolio/apps/angular-recruiter/jest.config.ts
+++ my-portfolio/apps/angular-recruiter/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'angular-recruiter',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../coverage/angular-recruiter',
   transform: {
